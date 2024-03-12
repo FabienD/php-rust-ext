b6f2d9b3f0d08b3fc9202c7bb625f714283b0d80
Fabien D <fabien.dosse@akeneo.com>
2024-03-12 08:43:47 +0000

    FFI (wip)

--- src/ffi.rs
+++ src/ffi.rs
@@ -23,12 +23,12 @@ pub struct AspellCanHaveError;
 
 extern "C" {
     pub fn new_aspell_config() -> *mut AspellConfig;
-    pub fn aspell_config_replace(config: *mut AspellConfig, mode: *const c_char, lang: *const c_char);
+    pub fn aspell_config_replace(config: *mut AspellConfig, key: *const c_char, value: *const c_char);
     pub fn delete_aspell_config(config: *mut AspellConfig);
     pub fn new_aspell_speller(config: *mut AspellConfig) -> *mut AspellCanHaveError;
-    //pub fn aspell_error_number(possible_err: *mut AspellCanHaveError) -> c_int;
+    pub fn aspell_error_number(possible_err: *mut AspellCanHaveError) -> c_int;
     pub fn to_aspell_speller(possible_err: *mut AspellCanHaveError) -> *mut AspellSpeller;
-    pub fn aspell_speller_check(speller: *mut AspellSpeller, word: *const c_char, size: c_int) -> c_int;
+    pub fn aspell_speller_check(speller: *mut AspellSpeller, word: *const c_char, word_size: c_int) -> c_int;
     //pub fn aspell_speller_suggest(speller: *mut AspellSpeller, word: *const c_char, size: c_int) -> *mut AspellWordList;
     //pub fn aspell_word_list_elements(suggestions: *mut AspellWordList) -> *mut AspellStringEnumeration;
     //pub fn delete_aspell_string_enumeration(elements: *mut AspellStringEnumeration);
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(windows, feature(abi_vectorcall))]
 use ext_php_rs::prelude::*;
+use ffi::AspellConfig;
 use libc::{c_char, c_int};
 
 mod ffi;
@@ -8,13 +9,14 @@ mod ffi;
 pub struct SpellResult {
     //misspelled: String,
     //pos: usize,
-    line: usize,
+    word: String,
+    //line: usize,
     //suggestions: Option<Vec<String>>,
 }
 
 #[php_class]
 pub struct SpellCheck {
-    spellchecker: *mut ffi::AspellSpeller,
+   spellchecker: *mut ffi::AspellSpeller,
 }
 
 #[php_impl]
@@ -33,18 +35,30 @@ impl SpellCheck {
 
         unsafe {
             ffi::aspell_config_replace(base_config, "lang".as_ptr() as *const c_char, locale.as_ptr() as *const c_char)
-        };
+        };       
+        // unsafe {
+        //     ffi::aspell_config_replace(base_config, "encoding".as_ptr() as *const c_char, "utf-8".as_ptr()  as *const c_char)
+        // }
+        
 
         let possible_err = unsafe {
             ffi::new_aspell_speller(base_config)
-        };       
+        };
 
-        if !possible_err.is_null() {
+        unsafe {
+            ffi::delete_aspell_config(base_config)
+        }
+
+        let error_number = unsafe {
+            ffi::aspell_error_number(possible_err)
+        };
+
+        if error_number == 0 {
             let spell_checker = unsafe {
                 ffi::to_aspell_speller(possible_err)
             };
 
-            if !spell_checker.is_null() {
+            if spell_checker.is_null() {
                 return Err(
                     PhpException::default("Aspell speller instance creation error.".to_string())
                 );
@@ -61,25 +75,17 @@ impl SpellCheck {
     }
 
     // Check misspelled word
-    pub fn check(&mut self, name: &str) -> Vec<SpellResult> {
-        let lines = name.lines();
-        let mut results: Vec<SpellResult> = Vec::new();
-
-        for (line_number, line_content) in lines.enumerate() {
-
-            let errors = unsafe {
-                ffi::aspell_speller_check(self.spellchecker, line_content.as_ptr() as *const c_char, line_content.len() as c_int)
-            };
-            
-            results.push(SpellResult {
-                //misspelled: e.misspelled,
-                //pos: e.position,
-                line: line_number,
-                // suggestions: Some(e.suggestions)
+    pub fn check(&mut self, name: &str) -> Option<SpellResult> {
+        
+        let check = unsafe {
+            ffi::aspell_speller_check(self.spellchecker, name.as_ptr() as *const c_char, name.len() as c_int)
+        };
+        if check == 0 {
+            return Some(SpellResult {
+                word: name.to_string(),
             });
         }
-
-        results
+        None
     }
 }
 
